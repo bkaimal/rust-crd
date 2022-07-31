b4bcac26d1c8c23628f735dc5e65781038724ebc
Biju Kaimal <biju@kaimal.net>
2022-07-31 20:27:51 +0530

    update to events

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use serde::{Deserialize, Serialize};
 use validator::Validate;
 use futures::{StreamExt, TryStreamExt};
 use kube::{
-    api::{Api, ListParams},
-    Client, CustomResource,
-    runtime::{watcher, WatchStreamExt}
+    api::{Api, ListParams, WatchEvent},
+    Client, CustomResource
 };
 
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, Validate, JsonSchema)]
@@ -22,9 +21,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Watch for changes to books in the configured namespace
     let books: Api<Book> = Api::default_namespaced(client.clone());
-    let mut apply_stream = watcher(books, ListParams::default()).applied_objects().boxed();
-    while let Some(event) = apply_stream.try_next().await? {
-        println!("saw apply to {}", event.spec.title);
+    let mut stream = books.watch(&ListParams::default(), "0").await?.boxed();
+    while let Some(event) = stream.try_next().await? {
+        match event {
+            WatchEvent::Added(book) => println!("ADDED: {} with name : {}", book.metadata.name.as_ref().unwrap().as_str(), book.spec.title),
+            WatchEvent::Modified(book) => println!("UPDATED: {} with name : {}", book.metadata.name.as_ref().unwrap().as_str(), book.spec.title),
+            WatchEvent::Deleted(book) => println!("DELETED: {}", book.metadata.name.as_ref().unwrap().as_str()),
+            WatchEvent::Error(e) => println!("ERROR: {} {} ({})", e.code, e.message, e.status),
+            _ => {}
+        };
     }
     Ok(())
 }
